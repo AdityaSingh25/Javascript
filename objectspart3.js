c24633fd741ef114ee8d65e091a829d13bd94ed0
AdityaSingh25 <richiaditya25@gmail.com>
2023-03-14 21:53:11 +0530

    14th march commit

--- objectspart3.js
+++ objectspart3.js
@@ -6,12 +6,21 @@ var User = function (firstName, courseCount) {
   };
 };
 
+User.prototype.getFirstName = function () {
+  console.log(`Your Name is : ${this.firstName}`);
+};
+
 var hitesh = new User("hitesh", 2);
 hitesh.getCourseCount();
 console.log(hitesh.firstName);
+hitesh.getFirstName();
 //console.log(hitesh);
-
+console.log("\n\n\n");
 var aditya = new User("Aditya", 4);
 aditya.getCourseCount();
 console.log(aditya.firstName);
+aditya.getFirstName();
 // console.log(aditya);
+console.log("\n\n\n");
+console.log("\n\n\n");
+console.log("\n\n\n");
