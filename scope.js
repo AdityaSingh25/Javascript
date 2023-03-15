f146e926c64b6dd258a86e47cbbb1f69776c7ac4
AdityaSingh25 <richiaditya25@gmail.com>
2023-03-15 22:38:50 +0530

    scope var let done

---
+++ scope.js
@@ -0,0 +1,14 @@
+//console.log(name);
+var name = "hitesh";
+
+if (true) {
+  let lastName = "Singh";
+}
+
+console.log(lastName);
+
+if (true) {
+  var lastName = "Singh";
+}
+
+console.log(lastName);
